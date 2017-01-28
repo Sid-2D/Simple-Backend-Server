ff697b49a9d88777026ed380aceb713038f6cd85
Siddharth Chopra <sid6327@gmail.com>
2017-01-28 12:39:10 +0530

    Allowing CORS

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = require('express')(),
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
 	res.send('Server working, available routes:<br>1. GET /course/:type<br>2. POST /student<br>3. POST /data');
 });
@@ -62,4 +68,4 @@ app.post('/data', function (req, res) {
 	});
 });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 1010);
